d6cd0200d1a21b1b846eee63ec5b46f23d104a56
AjayAnandhan <ajaya73635@gmail.com>
2023-07-16 06:02:21 +0530

    Task 11

--- src/Task11/Q7.java
+++ src/Task11/Q7.java
@@ -1,36 +1,27 @@
 package Task11;
 
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+class AgeValidator {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above.");
+        }
+    }
+}
+
 public class Q7 {
-
-	class InvalidAgeException extends Exception {
-	    public InvalidAgeException(String message) {
-	        super(message);
-	    }
-	}
-
-	public class Main {
-
-	    public void validateAge(int age) throws InvalidAgeException {
-	        if (age < 18) {
-	            throw new InvalidAgeException("Invalid age: Age must be 18 or above.");
-	        }
-	    }
-
-	    public void main(String[] args) {
-	        try {
-	            int age = 15; 
-
-
-	            validateAge(age);
-
-	            System.out.println("Age is valid. Continue with the program.");
-
-	        } catch (InvalidAgeException a) {
-
-	            System.out.println("Error: " + a.getMessage());
-	        }
-	    }
-	}
-
-
+    public static void main(String[] args) {
+        try {
+            int age = 16; // User input, for example purposes
+            AgeValidator.validateAge(age);
+            System.out.println("Age is valid.");
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
